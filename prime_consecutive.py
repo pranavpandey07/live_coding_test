a397e8222980d091af4626b38e5a98b0ce012a5a
pranavpandey07 <80163449+pranavpandey07@users.noreply.github.com>
2022-01-30 21:57:17 +0530

    Update prime_consecutive.py

--- prime_consecutive.py
+++ prime_consecutive.py
@@ -1,3 +1,5 @@
+
+#Question no1 solution.
 def consecutive_sum_prime_number(g):
     news=[]
     for i in range(1,g):
@@ -20,3 +22,25 @@ def consecutive_sum_prime_number(g):
 
 
 consecutive_sum_prime_number(100)
+
+
+#Question no 2 solution
+
+def strictly_smaller_strictly_bigger(nums):
+    new=[]
+    for i in range(0,len(nums)):
+        greater=0
+        smaller=0
+        for j in nums:
+            if nums[i]>j:
+                greater=greater+1
+            elif nums[i]<j:
+                smaller=smaller+1
+            else:
+                pass
+        if greater>=1 and smaller>=1:
+            new.append(i)
+    return len(new)
+      
+nums = [11,7,2,15]    
+strictly_smaller_strictly_bigger(nums)
